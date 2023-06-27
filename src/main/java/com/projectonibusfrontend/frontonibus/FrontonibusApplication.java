6558e4d4fa64283f59a851900c759670ab757738
KlaucioDamasceno <klaucio.etb@gmail.com>
2023-06-27 10:42:30 -0300

    create project front

---
+++ src/main/java/com/projectonibusfrontend/frontonibus/FrontonibusApplication.java
@@ -0,0 +1,15 @@
+package com.projectonibusfrontend.frontonibus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontonibusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontonibusApplication.class, args);
+	}
+
+
+
+}
---
+++ src/test/java/com/projectonibusfrontend/frontonibus/FrontonibusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectonibusfrontend.frontonibus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontonibusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
